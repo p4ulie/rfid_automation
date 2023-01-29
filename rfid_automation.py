5b7dd104d88da597cae79102d38c9ea53ead13b4
Pavol Antalík <pavol.antalik@gmail.com>
2023-01-29 22:16:16 +0100

    Add DB write support (SQLite)

--- rfid_automation.py
+++ rfid_automation.py
@@ -1,17 +1,18 @@
 import asyncio
 import configparser
-import datetime
+from datetime import datetime, timezone
 
 import tkinter as tk
 import tkinter.scrolledtext as tkst
 import tkinter.font as font
 from async_tkinter_loop import async_handler, async_mainloop
 
-# import numato_gpio
+import numato_gpio
 
 from evdev import InputDevice, categorize, ecodes
 from evdev_text_wrapper_asyncio import scancodes, capscodes, evdev_readline
 
+import sqlite3
 
 class Application(tk.Frame):
     """
@@ -29,7 +30,7 @@ class Application(tk.Frame):
         self.btn_cycle_start_stop = tk.Button(self,
                                               text="Tap to enable",
                                               command=async_handler(self.btn_cycle_start_stop_handler),
-                                              width=20, height=10,
+                                              width=10, height=5,
                                               font=font.Font(size=14),
                                               fg="white", activeforeground="white",
                                               activebackground="darkgreen", bg="darkgreen"
@@ -66,35 +67,50 @@ async def read_rfid(device, timeout):
 
     return result
 
+def db_log_entry(date, id):
+    print("Logged in DB")
+    db_connection.execute('INSERT INTO rfid values (?, ?)', (date, id) )
+    db_connection.commit()
+
 async def main_work_loop():
     while True:
-
         if app.cycle_start_stop:
-            # print("Worker invoked - app cycle is started")
-            clean_buffer = await read_rfid(rfid_reader, 0.5)
-            print("Clean buffer: %s" % clean_buffer)
-
-            current_datetime = datetime.datetime.now()
+            current_datetime = datetime.now()
             current_datetime_formatted = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
 
-            # app.IO_text.delete (1.0, tk.END)
+            # read the value to "clean the buffer"
+            clean_buffer = await read_rfid(rfid_reader, 0.5)
 
-            conveyor_sensor = 1
-            # conveyorSensor = numato_gpio.read(config['GpioDeviceSettings']['PortConveyorSensor'])
+            conveyor_sensor_port = numato_gpio.read(config['GpioDeviceSettings']['PortConveyorSensor'])
+            conveyor_sensor = numato_gpio.read(conveyor_sensor_port)
+            # conveyor_sensor = 1
+
+            app.IO_text.insert(tk.END, "%s: Conveyor sensor value: %s\n" % (current_datetime_formatted, conveyor_sensor))
+
             if conveyor_sensor == 1:
-                app.IO_text.insert(tk.END, "%s: RFID tag detected\n" % current_datetime_formatted)
+                # app.IO_text.delete (1.0, tk.END)
 
+                app.IO_text.insert(tk.END, "%s: RFID tag detected\n" % current_datetime_formatted)
                 app.IO_text.insert(tk.END, "%s: Read output of RFID reader\n" % current_datetime_formatted)
 
+                # move the scrolledtext widget position to end
                 app.IO_text.see("end")
 
+                # read the RFID tag's ID
                 rfid_tag_id = await read_rfid(rfid_reader, rfid_reader_timeout)
-                app.IO_text.insert(tk.END, "%s: RFID tag: %s\n" % (current_datetime_formatted, rfid_tag_id))
+                # rfid_tag_id = "999000000020044"
 
-                app.IO_text.insert(tk.END, "\n")
-            else:
-                app.IO_text.insert(tk.END, "%s: RFID tag not detected\n" % current_datetime_formatted)
+                db_log_entry(datetime.now(timezone.utc), rfid_tag_id)
 
+                if rfid_tag_id != "timeout":
+                    app.IO_text.insert(tk.END, "%s: RFID tag: %s\n" % (current_datetime_formatted, rfid_tag_id))
+                else:
+                    app.IO_text.insert(tk.END, "%s: RFID tag not detected\n" % current_datetime_formatted)
+
+            # else:
+
+            app.IO_text.insert(tk.END, "\n")
+            # move the scrolledtext widget position to end
             app.IO_text.see("end")
 
         await asyncio.sleep(0.1)
@@ -104,26 +120,39 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('settings.ini')
 
-    # numato_gpio = numato_gpio.numato_gpio(
-    #     config['GpioDeviceSettings']['Name'],
-    #     config['GpioDeviceSettings']['Speed'],
-    #     timeout=1)
-    #
+    db_file = config['Database_Sqlite']['File']
+    print("Open database connection:", db_file)
+    db_connection = sqlite3.connect(db_file)
+    # db_cursor = db_connection.cursor()
+    db_connection.execute("""CREATE TABLE IF NOT EXISTS rfid(
+                              date text PRIMARY KEY,
+                              id varchar(15) NOT NULL)"""
+                          )
+
+    numato_gpio = numato_gpio.numato_gpio(
+        config['GpioDeviceSettings']['Name'],
+        config['GpioDeviceSettings']['Speed'],
+        com_timeout=1)
+
+    # initialize the RFID reader and grab the device so system won't catch events
     rfid_reader = InputDevice(config['RfidReaderDeviceSettings']['Name'])
     rfid_reader_timeout = int(config['RfidReaderDeviceSettings']['Timeout'])
     rfid_reader.grab()
-    #
-    # # 1 = unmask, 0 = mask
-    # numato_gpio.iomask(int("1111111111111111",2))
-    # # 1 = input, 0 = output
-    # numato_gpio.iodir(int("1111111111111111",2))
-    #
+
+    # 1 = unmask, 0 = mask
+    numato_gpio.iomask(int("0000000001100001",2))
+    # 1 = input, 0 = output
+    numato_gpio.iodirall(int("0000000000000001",2))
+
     window = tk.Tk()
-
     app = Application(master=window)
-
     window.minsize(width=1000, height=250)
 
+    # start main work loop
     asyncio.get_event_loop_policy().get_event_loop().create_task(main_work_loop())
 
+    # start GUI event processing loop
     async_mainloop(window)
+
+    db_connection.close()
+    print("Database connection closed.")
